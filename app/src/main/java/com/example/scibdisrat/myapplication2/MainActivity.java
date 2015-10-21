a28b3adaab10bf60c4e3efbb54287579860dbff3
Isratmity01 <mity.crystal@gmail.com>
2015-10-21 16:43:01 +0600

    added fusedlocation api, fixed for android kitkat

--- app/src/main/java/com/example/scibdisrat/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/scibdisrat/myapplication2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.scibdisrat.myapplication2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -9,12 +10,12 @@ import android.widget.ImageButton;
  * @author SCIBBDISRAT
  */
 public class  MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d(TAG,"mity1 ...............................");
         final ImageButton imbtn = (ImageButton) findViewById(R.id.imageButton);
         imbtn.setOnClickListener(this);
     }
--- app/src/main/java/com/example/scibdisrat/myapplication2/SecondActivity.java
+++ app/src/main/java/com/example/scibdisrat/myapplication2/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.scibdisrat.myapplication2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,12 +11,13 @@ import android.widget.Button;
  */
 
 public class  SecondActivity extends AppCompatActivity implements View.OnClickListener {
-
+    private static final String TAG = "SecondActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        Log.d(TAG,"mity2");
         final Button btn = (Button) findViewById(R.id.button);
 
         final Button btn2 = (Button) findViewById(R.id.button2);
@@ -28,7 +30,7 @@ public class  SecondActivity extends AppCompatActivity implements View.OnClickLi
         {
 
             if (v.getId() == R.id.button) {
-                Intent intent = new Intent(this, HospitalActivity.class);
+                Intent intent = new Intent(this, LocationActivity.class);
                 startActivity(intent);
 
             } else if (v.getId() == R.id.button2) {
